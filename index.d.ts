0e5d5a7c7a647c82e62b0f71ed1886dff7051987
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2023-01-11 16:55:24 +0100

    added intersection module and updated others

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,39 @@
+declare module "intersection" {
+
+    // shared between all three modules
+
+    export interface ActionType {
+        target: {
+            localName: string;
+            id: string;
+            className: string;
+        },
+        type: string;
+    }
+    
+    export interface ErrorLogType {
+        message: string,
+        name: string,
+        stack: string,
+        actions: ActionType[],
+        browserVersion: string,
+        timestamp: number
+    }
+    
+    export interface ExtendedErrorLogType extends ErrorLogType {
+        appId: string,
+        sessionId: string
+    }
+
+}
+
 declare module "delivery-backend" {
 
     // shared between error delivery module and back end
 
     import { Query, Send } from 'express-serve-static-core';
     import * as express from 'express';
+    import { ErrorLogType, ExtendedErrorLogType } from 'intersection';
 
     export interface TypedRequest<T, U extends Query> extends express.Request{
         body: T,
@@ -24,18 +54,12 @@ declare module "delivery-backend" {
         appSecret: string
     }
 
-    export interface ErrorReportInterface {
-        message: string,
-        name: string,
-        stack: string,
-        actions: object[],
-        browserVersion: string,
-        timestamp: number
+    export interface ErrorReportInterface extends ErrorLogType {
+        // preserve local name to avoid breaking change
     }
 
-    export interface RequestBodyInterface extends ErrorReportInterface {
-        appId: string,
-        sessionId: string
+    export interface RequestBodyInterface extends ExtendedErrorLogType {
+        // preserve local name to avoid breaking change
     }
 
     export type ErrorResponseType = Error & {statusCode: number};
@@ -51,24 +75,30 @@ declare module "frontend-backend" {
 
     // shared between back end and front end
 
-    export interface Action {
-        target: {
-            localName: string;
-            id: string;
-            className: string;
-        },
-        type: string;
+    import { ActionType, ExtendedErrorLogType } from 'intersection';
+
+    export interface Action extends ActionType {
+        // preserve local name to avoid breaking change
     }
 
-    export interface ErrorLogInterface {
-        appId: string,
-        sessionId: string,
-        message: string,
+    export interface AtomicViewType extends ExtendedErrorLogType {
+        // preserve local name to avoid breaking change
+    }
+
+    export interface ErrorViewType {
         name: string,
+        message: string,
         stack: string,
-        actions: Action[],
-        browserVersion: string,
-        timestamp: Date
+        browserVersion: string[],
+        totalErrors: number,
+        totalSessions: number
+    }
+
+    export interface SessionViewType {
+        sessionId: string,
+        date: Date,
+        totalErrors: number,
+        errors: ExtendedErrorLogType[]
     }
 
     export interface AdminAuthRequest {
